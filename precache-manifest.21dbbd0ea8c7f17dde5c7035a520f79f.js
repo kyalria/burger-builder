cb20a919829a4e5301c39c89b2ba62c0d6de1b54
Ria kyal <riakyal@gmail.com>
2020-05-22 20:12:33 +0530

    Updates

---
+++ precache-manifest.21dbbd0ea8c7f17dde5c7035a520f79f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7da06746c5139d14d8e13e7d6d64ebb",
+    "url": "/burger-builder-react-app/index.html"
+  },
+  {
+    "revision": "380bb3de115ea753c7dc",
+    "url": "/burger-builder-react-app/static/css/main.c49331d4.chunk.css"
+  },
+  {
+    "revision": "9de3bdcb4f32c1fa4418",
+    "url": "/burger-builder-react-app/static/js/2.bf6515c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger-builder-react-app/static/js/2.bf6515c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "380bb3de115ea753c7dc",
+    "url": "/burger-builder-react-app/static/js/main.b769f4cc.chunk.js"
+  },
+  {
+    "revision": "cce949d037de0e4068c3",
+    "url": "/burger-builder-react-app/static/js/runtime-main.32f60879.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder-react-app/static/media/burger.b8503d26.png"
+  }
+]);
